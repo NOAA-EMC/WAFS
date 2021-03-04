552ff4aff7a5fdb1e6552aaced4bab5f77a9a6ba
Rahul Mahajan <aerorahul@users.noreply.github.com>
2021-03-04 18:33:43 -0500

    build wafs utilities with cmake

---
+++ sorc/build_wafs_cmake.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -eux
+
+# detect machine
+mac=$(hostname | cut -c1-1)
+mac2=$(hostname | cut -c1-2)
+if [ $mac = v -o $mac = m  ] ; then # For Dell
+  machine=dell
+  set +x
+  . $MODULESHOME/init/bash
+  set -x
+elif [ $mac2 = hf ] ; then          # For Hera
+  machine=hera
+  set +x
+  . /etc/profile
+  . /etc/profile.d/modules.sh
+  set -x
+elif [ $mac = O ] ; then            # For Orion
+  machine=orion
+  set +x
+  . /etc/profile
+  set -x
+else
+  machine=NULL
+fi
+
+if [ $machine = "dell" -o $machine = "hera" -o $machine = "orion" ]; then
+  moduledir=`dirname $(readlink -f ../modulefiles/wafs)`
+  set +x
+  module use ${moduledir}
+  module load wafs/wafs_v7.0.0-${machine}
+  module list
+  set -x
+fi
+
+[[ -d build  ]] && rm -rf build
+mkdir -p build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=../../ -DCMAKE_INSTALL_BINDIR=exec ..
+make -j ${BUILD_JOBS:-4} VERBOSE=${BUILD_VERBOSE:-}
+make install
