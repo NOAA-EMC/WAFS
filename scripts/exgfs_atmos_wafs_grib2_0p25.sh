b559147791de6cbdc9a147394dfd9546a4642da1
Yali Mao <yali.mao@noaa.gov>
2021-04-26 19:09:16 +0000

    Updated with GFSv16 implementation changes Updated with bugzilla #1209 fix

--- scripts/exgfs_atmos_wafs_grib2_0p25.sh
+++ scripts/exgfs_atmos_wafs_grib2_0p25.sh
@@ -79,11 +79,14 @@ do
   opt24=' -set_bitmap 1 -set_grib_max_bits 16 '
   newgrid="latlon 0:1440:0.25 90:721:-0.25"
 
-  ###### Step 1: Collect fields of EDPARM and ICESEV, convert to 1/4 deg for US stakeholders ######
-  criteria=":EDPARM:|:ICESEV:|parm=37:"
-  $WGRIB2 $wafs2 | egrep $criteria | egrep -v ":70 mb:" | $WGRIB2 -i $wafs2 -grib tmp_wafs1_grb2
+  ###### Step 1: Collect fields of EDPARM and ICESEV for blending, plus CAT WMT for US stakeholders, convert to 1/4 deg ######
+  criteria1=":EDPARM:|:ICESEV:|parm=37:"
+  criteria2=":CATEDR:|:MWTURB:"
+  $WGRIB2 $wafs2 | egrep "${criteria1}|$criteria2" | egrep -v ":70 mb:" | $WGRIB2 -i $wafs2 -grib tmp_wafs1_grb2
   $WGRIB2 tmp_wafs1_grb2 $opt1 $opt21 $opt22 $opt23 $opt24 -new_grid $newgrid gfs.t${cyc}z.wafs_0p25.f${ffhr}.grib2
   $WGRIB2 -s gfs.t${cyc}z.wafs_0p25.f${ffhr}.grib2 > gfs.t${cyc}z.wafs_0p25.f${ffhr}.grib2.idx
+
+  $WGRIB2 gfs.t${cyc}z.wafs_0p25.f${ffhr}.grib2 | egrep -v $criteria2 | $WGRIB2 -i gfs.t${cyc}z.wafs_0p25.f${ffhr}.grib2 -grib tmp_wafs_grb2.0p25.forblend
 
   ###### Step 2: Collect CB fields, convert to 1/4 deg  ######
   criteria=":CBHE:|:ICAHT:"
@@ -91,7 +94,7 @@ do
   $WGRIB2 tmp_wafs2_grb2 $opt1 $opt21 $opt22 $opt23 $opt24 -new_grid $newgrid tmp_wafs_grb2.0p25
 
   ###### Step 3: Combine CB, EDPARM and ICESEV together for blending ######
-  cat gfs.t${cyc}z.wafs_0p25.f${ffhr}.grib2 >> tmp_wafs_grb2.0p25
+  cat tmp_wafs_grb2.0p25.forblend >> tmp_wafs_grb2.0p25
 
   ###### Step 4: Change to grib2 template 5.40 and relabel pressure levels to exact numbers  ######
   # Relabelling should be removed when UPP WAFS output on the exact pressure levels
