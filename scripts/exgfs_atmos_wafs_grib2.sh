80f008c96316c59113e489dfc02d3ac81a34fd5a
yali mao <yali.mao@clogin01.cactus.wcoss2.ncep.noaa.gov>
2024-03-18 20:39:16 +0000

    Cleaned up WAFS legacy files that are no longer needed - Removed outdated fix/wafs parm/wafs files - Removed 1.25 hazard grib2 source code sorc/wafs_awc_wafavn.fd sorc/wafs_blending.fd sorc/wafs_setmissing.fd, updated sorc/build_wafs.sh sorc/CMakeLists.txt - Removed 1.25 hazard grib2 parts from parm/script/ush files - Removed 1.25 blend parm/script/ush file - Removed ICAO2023=no related lines, updated related scripts/ush - Removed GRIB octant related ush file and updated the script - Renamed some fix files for better organizing the fix filenames Updated drivers for GFSv16.3 after this clean-up

--- scripts/exgfs_atmos_wafs_grib2.sh
+++ scripts/exgfs_atmos_wafs_grib2.sh
@@ -96,9 +96,9 @@ fi
 #---------------------------
 # 1) Grib2 data for FAA
 #---------------------------
-$WGRIB2 $master2 | grep -F -f $FIXgfs/faa_gfsmaster.grb2.list | $WGRIB2 -i $master2 -grib tmpfile_gfsf${fcsthrs}
+$WGRIB2 $master2 | grep -F -f $FIXgfs/grib2_gfs_awf_master.list | $WGRIB2 -i $master2 -grib tmpfile_gfsf${fcsthrs}
 # F006 master file has two records of 0-6 hour APCP and ACPCP each, keep only one
-# FAA APCP ACPCP: included every 6 forecast hour (0, 48], every 12 forest hour [48, 72] (controlled by $FIXgfs/faa_gfsmaster.grb2.list)
+# FAA APCP ACPCP: included every 6 forecast hour (0, 48], every 12 forest hour [48, 72] (controlled by $FIXgfs/grib2_gfs_awf_master3d.list)
 if [ $fcsthrs -eq 6 ] ; then
     $WGRIB2 tmpfile_gfsf${fcsthrs} -not "(APCP|ACPCP)" -grib tmp.grb2
     $WGRIB2 tmpfile_gfsf${fcsthrs} -match APCP -append -grib tmp.grb2 -quit
@@ -129,113 +129,35 @@ if [ $wafs_timewindow = 'yes' ] ; then
 #---------------------------
 # 2) traditional WAFS fields
 #---------------------------
-    if [ "$ICAO2023" = 'yes' ] ; then
-	# 3D data from $wafs2, on exact model pressure levels
-	$WGRIB2 $wafs2 | grep -F -f $FIXgfs/wafs_gfsmaster.grb2.list | $WGRIB2 -i $wafs2 -grib tmpfile_gfsf${fcsthrs}
-	# 2D data from $master2
-	tail -5 $FIXgfs/wafs_gfsmaster.grb2.list > wafs_gfsmaster.grb2.list.2D
-	$WGRIB2 $master2 | grep -F -f wafs_gfsmaster.grb2.list.2D | $WGRIB2 -i $master2 -grib tmpfile_gfsf${fcsthrs}.2D
-	# Complete list of WAFS data
-	cat tmpfile_gfsf${fcsthrs}.2D >> tmpfile_gfsf${fcsthrs}
-	# WMO header
-	cp $FIXgfs/grib2_gfs_wafsf${fcsthrs}.45 wafs_wmo_header45
-    else
-	$WGRIB2 $master2 | grep -F -f $FIXgfs/legend/wafs_gfsmaster.grb2.list | $WGRIB2 -i $master2 -grib tmpfile_gfsf${fcsthrs}
-	# WMO header
-	cp $FIXgfs/legend/grib2_gfs_wafsf${fcsthrs}.45 wafs_wmo_header45
-    fi
+    # 3D data from $wafs2, on exact model pressure levels
+    $WGRIB2 $wafs2 | grep -F -f $FIXgfs/grib2_gfs_wafs_wafsmaster.list | $WGRIB2 -i $wafs2 -grib tmpfile_gfsf${fcsthrs}
+    # 2D data from $master2
+    tail -5 $FIXgfs/grib2_gfs_wafs_wafsmaster.list > grib2_gfs_wafs_wafsmaster.list.2D
+    $WGRIB2 $master2 | grep -F -f grib2_gfs_wafs_wafsmaster.list.2D | $WGRIB2 -i $master2 -grib tmpfile_gfsf${fcsthrs}.2D
+    # Complete list of WAFS data
+    cat tmpfile_gfsf${fcsthrs}.2D >> tmpfile_gfsf${fcsthrs}
+    # WMO header
+    cp $FIXgfs/grib2_gfs_wafsf${fcsthrs}.45 wafs_wmo_header45
     # U V will have the same grid message number by using -ncep_uv.
     # U V will have the different grid message number without -ncep_uv.
     $WGRIB2 tmpfile_gfsf${fcsthrs} \
             -set master_table 6 \
             -new_grid_winds earth -set_grib_type jpeg \
             -new_grid_interpolation bilinear -if ":(UGRD|VGRD):max wind" -new_grid_interpolation neighbor -fi \
-            -new_grid latlon 0:288:1.25 90:145:-1.25 gfs.t${cyc}z.wafs_grb45f${fcsthrs}.nouswafs.grib2
-    $WGRIB2 -s gfs.t${cyc}z.wafs_grb45f${fcsthrs}.nouswafs.grib2 > gfs.t${cyc}z.wafs_grb45f${fcsthrs}.nouswafs.grib2.idx
+            -new_grid latlon 0:288:1.25 90:145:-1.25 gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2
+    $WGRIB2 -s gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2 > gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2.idx
 
     # For WAFS, add WMO header. Processing WAFS GRIB2 grid 45 for ISCS and WIFS
     export pgm=$TOCGRIB2
     . prep_step
     startmsg
-    export FORT11=gfs.t${cyc}z.wafs_grb45f${fcsthrs}.nouswafs.grib2
+    export FORT11=gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2
     export FORT31=" "
     export FORT51=grib2.t${cyc}z.wafs_grbf${fcsthrs}.45
     $TOCGRIB2 < wafs_wmo_header45 >> $pgmout 2> errfile
     err=$?;export err ;err_chk
     echo " error from tocgrib=",$err
 
-    if [ "$ICAO2023" = 'yes' ] ; then
-	mv gfs.t${cyc}z.wafs_grb45f${fcsthrs}.nouswafs.grib2 gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2
-	mv gfs.t${cyc}z.wafs_grb45f${fcsthrs}.nouswafs.grib2.idx gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2.idx
-    else
-#---------------------------
-# 3) WAFS hazard dataset (cb, icing, turbulence), will be stoped in 2023
-#---------------------------
-
-	cp $PARMgfs/legend/wafs_awc_wafavn.grb2.cfg waf.cfg
-
-	date
-
-	# For high resolution maste file, run time of awc_wafavn is 20 seconds for 1440 x 721, 
-	# 3 minutes for new 3072 x 1536 master file for each forecast.
-	# To reduce the time, will extract the required fields from master file and wafs files,
-	# then convert to 1440 x 721.
-	$WGRIB2 -npts $master2 > master.npts
-	npts=`head -n1 master.npts | cut -d'=' -f2`
-	newgrid="latlon 0:1440:0.25 90:721:-0.25"
-	if [ $npts -gt 1038240 ] ; then
-	    regrid_options="bilinear $newgrid"
-	else
-	    regrid_options=""
-	fi
-
-	options='-set_bitmap 1 -set_grib_type same -new_grid_winds earth'
-	# 2D inputs for WAFS from master file, reference to type(pdt_t) parameters sorc/wafs_awc_wafavn.fd/waf_grib2.f90
-	criteria1=":PRES:surface:|:PRES:convective|:CPRAT:.*hour.{1}ave"
-	$WGRIB2 $master2 | egrep "$criteria1" |  $WGRIB2 -i $master2 -grib master.fields
-	criteria=":HGT:.* mb:|:TMP:.* mb:|:UGRD:.* mb:|VGRD:.* mb:|:ICIP:.* mb:"
-	$WGRIB2 $icao2 | egrep "$criteria" |  $WGRIB2 -i $icao2 -grib wafs.fields
-	cat master.fields wafs.fields > masterfilef${fcsthrs}.new
-	rm master.fields wafs.fields
-	$WGRIB2  masterfilef${fcsthrs}.new $options -set master_table 6 -new_grid_interpolation bilinear -new_grid latlon 0:1440:0.25 90:721:-0.25 masterfilef${fcsthrs}
-
-	export pgm=wafs_awc_wafavn
-	. prep_step
-
-	startmsg
-	$EXECgfs/$pgm -c waf.cfg -i masterfilef${fcsthrs} -o tmpfile_icaof${fcsthrs} icng cat cb  >> $pgmout  2> errfile
-	export err=$?; err_chk
-
-	# To avoid interpolation of missing value (-0.1 or -1.0, etc), use neighbor interpolation instead of bilinear interpolation
-	$WGRIB2 tmpfile_icaof${fcsthrs} -set_grib_type same -new_grid_winds earth \
-		-new_grid_interpolation bilinear -if ":CBHE:" -new_grid_interpolation neighbor -fi \
-		-new_grid latlon 0:288:1.25 90:145:-1.25 tmpfile_icao_grb45f${fcsthrs}
-	# after grid conversion by wgrib2, even with neighbor interpolation, values may still be mislead by noises, epescially 
-	# the ref_value is not zero according to DST template 5.XX. Solution: rewrite and round those special meaning values
-	export pgm=wafs_setmissing
-	. prep_step
-	$EXECgfs/wafs_setmissing tmpfile_icao_grb45f${fcsthrs} tmpfile_icao_grb45f${fcsthrs}.setmissing
-	mv tmpfile_icao_grb45f${fcsthrs}.setmissing tmpfile_icao_grb45f${fcsthrs}
-
-#---------------------------
-# 4) combine hazard and traditional WAFS fields
-#---------------------------
-	cat gfs.t${cyc}z.wafs_grb45f${fcsthrs}.nouswafs.grib2 tmpfile_icao_grb45f${fcsthrs} > gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2
-	# Stop support grib1 data beginning from 2023
-	#  $CNVGRIB -g21 gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2 gfs.t${cyc}z.wafs_grb45f${fcsthrs}
-	$WGRIB2 -s gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2 > gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2.idx
-
-	# Processing WAFS GRIB2 grid 45 (Icing, TB, CAT) for WIFS
-	export pgm=$TOCGRIB2
-	. prep_step
-	startmsg
-	export FORT11=gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2
-	export FORT31=" "
-	export FORT51=grib2.t${cyc}z.wafs_grb_wifsf${fcsthrs}.45
-	$TOCGRIB2 <  $FIXgfs/legend/grib2_gfs_wafs_wifs_f${fcsthrs}.45 >> $pgmout 2> errfile
-	err=$?;export err ;err_chk
-	echo " error from tocgrib=",$err
-    fi # ICAO2023
 fi # wafs_timewindow
 
 if [ $SENDCOM = "YES" ] ; then
@@ -250,12 +172,6 @@ if [ $SENDCOM = "YES" ] ; then
 
     # WAFS data
     if [ $wafs_timewindow = 'yes' ] ; then
-	if [ ! "$ICAO2023" = 'yes' ] ; then
-	    mv gfs.t${cyc}z.wafs_grb45f${fcsthrs}.nouswafs.grib2 $COMOUT/gfs.t${cyc}z.wafs_grb45f${fcsthrs}.nouswafs.grib2
-	    mv gfs.t${cyc}z.wafs_grb45f${fcsthrs}.nouswafs.grib2.idx $COMOUT/gfs.t${cyc}z.wafs_grb45f${fcsthrs}.nouswafs.grib2.idx
-	    # Retiring grib1 data
-	    # mv gfs.t${cyc}z.wafs_grb45f${fcsthrs}  $COMOUT/gfs.t${cyc}z.wafs_grb45f${fcsthrs}
-	fi
 	mv gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2 $COMOUT/gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2
 	mv gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2.idx $COMOUT/gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2.idx
     fi
@@ -268,9 +184,6 @@ if [ $SENDCOM = "YES" ] ; then
 
     if [ $wafs_timewindow = 'yes' ] ; then
 	mv grib2.t${cyc}z.wafs_grbf${fcsthrs}.45  $PCOM/grib2.t${cyc}z.wafs_grbf${fcsthrs}.45
-        if [ ! "$ICAO2023" = 'yes' ] ; then
-	    mv grib2.t${cyc}z.wafs_grb_wifsf${fcsthrs}.45  $PCOM/grib2.t${cyc}z.wafs_grb_wifsf${fcsthrs}.45
-	fi
     fi
 fi
 
@@ -284,13 +197,7 @@ if [ $SENDDBN = "YES" ] ; then
 #    Distribute Data to WOC
 #  
     if [ $wafs_timewindow = 'yes' ] ; then
-        if [ ! "$ICAO2023" = 'yes' ] ; then
-	    $DBNROOT/bin/dbn_alert MODEL GFS_WAFSA_GB2 $job $PCOM/grib2.t${cyc}z.wafs_grb_wifsf${fcsthrs}.45
-	    $DBNROOT/bin/dbn_alert MODEL GFS_WAFSA_GB2 $job $COMOUT/gfs.t${cyc}z.wafs_grb45f${fcsthrs}.nouswafs.grib2
-	    $DBNROOT/bin/dbn_alert MODEL GFS_WAFSA_GB2_WIDX $job $COMOUT/gfs.t${cyc}z.wafs_grb45f${fcsthrs}.nouswafs.grib2.idx
-	else
-	    $DBNROOT/bin/dbn_alert MODEL GFS_WAFS_1P25_GB2 $job $COMOUT/gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2
-	fi
+	$DBNROOT/bin/dbn_alert MODEL GFS_WAFS_1P25_GB2 $job $COMOUT/gfs.t${cyc}z.wafs_grb45f${fcsthrs}.grib2
 #
 #       Distribute Data to TOC TO WIFS FTP SERVER (AWC)
 #
--- scripts/exgfs_atmos_wafs_grib2_0p25.sh
+++ scripts/exgfs_atmos_wafs_grib2_0p25.sh
@@ -104,7 +104,7 @@ newgrid="latlon 0:1440:0.25 90:721:-0.25"
 # WAFS 3D data
 $WGRIB2 $wafs2 $opt1 $opt21 $opt22 $opt23 $opt24 -new_grid $newgrid tmp_wafs_0p25.grb2
 # Master 2D data
-$WGRIB2 $master2 | grep -F -f $FIXgfs/gfs_master.grb2_0p25.list \
+$WGRIB2 $master2 | grep -F -f $FIXgfs/grib2_0p25_gfs_master2d.list \
     | $WGRIB2 -i $master2 -set master_table 25 -grib tmp_master.grb2
 $WGRIB2 tmp_master.grb2 $opt1 $opt21 ":(UGRD|VGRD):max wind" $opt23 $opt24 -new_grid $newgrid tmp_master_0p25.grb2
 
@@ -119,100 +119,28 @@ cat tmp_master_0p25.grb2 >> tmp.gfs.t${cyc}z.wafs_0p25.f${ffhr}.grib2
 mv tmp.gfs.t${cyc}z.wafs_0p25.f${ffhr}.grib2 gfs.t${cyc}z.wafs_0p25.f${ffhr}.grib2
 $WGRIB2 -s gfs.t${cyc}z.wafs_0p25.f${ffhr}.grib2 > gfs.t${cyc}z.wafs_0p25.f${ffhr}.grib2.idx
 
-if [ "$ICAO2023" = 'yes' ] ; then
-    if [ $hazard_timewindow = 'yes' ] ; then
+if [ $hazard_timewindow = 'yes' ] ; then
 #---------------------------
 # Product 2: For AWC and Delta airline: EDPARM CAT MWT ICESEV CB  gfs.tHHz.awf_0p25.fFFF.grib2
 #---------------------------
-	criteria1=":EDPARM:|:ICESEV:|parm=37:"
-	criteria2=":CATEDR:|:MWTURB:"
-	criteria3=":CBHE:|:ICAHT:"
-	$WGRIB2 tmp_wafs_0p25.grb2 | egrep "${criteria1}|$criteria2|$criteria3" \
-	    | $WGRIB2 -i tmp_wafs_0p25.grb2 -grib gfs.t${cyc}z.awf_0p25.f${ffhr}.grib2
-	$WGRIB2 -s gfs.t${cyc}z.awf_0p25.f${ffhr}.grib2 > gfs.t${cyc}z.awf_0p25.f${ffhr}.grib2.idx
+    criteria1=":EDPARM:|:ICESEV:|parm=37:"
+    criteria2=":CATEDR:|:MWTURB:"
+    criteria3=":CBHE:|:ICAHT:"
+    $WGRIB2 tmp_wafs_0p25.grb2 | egrep "${criteria1}|$criteria2|$criteria3" \
+	| $WGRIB2 -i tmp_wafs_0p25.grb2 -grib gfs.t${cyc}z.awf_0p25.f${ffhr}.grib2
+    $WGRIB2 -s gfs.t${cyc}z.awf_0p25.f${ffhr}.grib2 > gfs.t${cyc}z.awf_0p25.f${ffhr}.grib2.idx
 
 #---------------------------
 # Product 3: WAFS unblended EDPARM, ICESEV, CB (No CAT MWT) gfs.tHHz.wafs_0p25_unblended.fFF.grib2
 #---------------------------
-	$WGRIB2 tmp_wafs_0p25.grb2 | grep -F -f $FIXgfs/gfs_wafs.grb2_0p25.list \
-	    | $WGRIB2 -i tmp_wafs_0p25.grb2 -set master_table 25 -grib tmp_wafs_0p25.grb2.forblend
+    $WGRIB2 tmp_wafs_0p25.grb2 | grep -F -f $FIXgfs/grib2_0p25_gfs_hazard.list \
+	| $WGRIB2 -i tmp_wafs_0p25.grb2 -set master_table 25 -grib tmp_wafs_0p25.grb2.forblend
 
-	# Convert template 5 to 5.40
-	#$WGRIB2 tmp_wafs_0p25.grb2.forblend -set_grib_type jpeg -grib_out gfs.t${cyc}z.wafs_0p25_unblended.f${ffhr2}.grib2
-	mv tmp_wafs_0p25.grb2.forblend gfs.t${cyc}z.wafs_0p25_unblended.f${ffhr2}.grib2
-	$WGRIB2 -s gfs.t${cyc}z.wafs_0p25_unblended.f${ffhr2}.grib2 > gfs.t${cyc}z.wafs_0p25_unblended.f${ffhr2}.grib2.idx
-    fi
-
-else # icao2 on standard atmosphere pressure levels, relabeled to ICAO pressure levels every 25mb
-
-    # Collect CB fields, convert to 1/4 deg
-    criteria=":CBHE:|:ICAHT:"
-    $WGRIB2 $wafs2 | egrep $criteria | $WGRIB2 -i $wafs2 -grib tmp_wafs2_grb2
-    $WGRIB2 tmp_wafs2_grb2 $opt1 $opt21 $opt22 $opt23 $opt24 -new_grid $newgrid tmp_wafs_grb2.0p25
-
-#---------------------------
-# Product 2 (before 2023): For AWC and Delta airline: EDPARM CAT MWT ICESEV CB  gfs.tHHz.awf_0p25.fFFF.grib2
-#---------------------------
-    criteria1=":EDPARM:|:ICESEV:|parm=37:"
-    criteria2=":CATEDR:|:MWTURB:"
-    $WGRIB2 $icao2 | egrep "${criteria1}|$criteria2" | egrep -v ":70 mb:" | $WGRIB2 -i $icao2 -grib tmp_icao2_grb2
-    $WGRIB2 tmp_icao2_grb2 $opt1 $opt21 $opt22 $opt23 $opt24 -new_grid $newgrid gfs.t${cyc}z.awf_0p25.f${ffhr}.grib2
-    cat tmp_wafs_grb2.0p25 >> gfs.t${cyc}z.awf_0p25.f${ffhr}.grib2
-    $WGRIB2 -s gfs.t${cyc}z.awf_0p25.f${ffhr}.grib2 > gfs.t${cyc}z.awf_0p25.f${ffhr}.grib2.idx
-
-#---------------------------
-# Product 3 (before 2023): WAFS unblended EDPARM, ICESEV, CB (No CAT MWT) gfs.tHHz.wafs_0p25_unblended.fFF.grib2
-#---------------------------
-    $WGRIB2 gfs.t${cyc}z.awf_0p25.f${ffhr}.grib2 | egrep -v $criteria2 \
-	| $WGRIB2 -i gfs.t${cyc}z.awf_0p25.f${ffhr}.grib2 -grib tmp_awf_grb2.0p25.forblend
-
-    cat tmp_awf_grb2.0p25.forblend >> tmp_wafs_grb2.0p25
-
-    # Relabel pressure levels to exact numbers and change to grib2 template 5.40
-    # (Relabelling should be removed when UPP WAFS output on the exact pressure levels)
-    export pgm=wafs_grib2_0p25
-    . prep_step
-    startmsg
-    $EXECgfs/$pgm tmp_wafs_grb2.0p25 tmp_0p25_exact.grb2 >> $pgmout 2> errfile
-    export err=$?; err_chk
-# WGRIB2 set_lev doesn't work well. The output will fail on DEGRIB2 and
-# it change values of octet 30, octet 31-34 of template 4 from 0 to undefined values
-#  $WGRIB2 tmp_0p25_ref.grb2 \
-#      -if ":100 mb" -set_lev "100.4 mb" -fi \
-#      -if ":125 mb" -set_lev "127.7 mb" -fi \
-#      -if ":150 mb" -set_lev "147.5 mb" -fi \
-#      -if ":175 mb" -set_lev "178.7 mb" -fi \
-#      -if ":200 mb" -set_lev "196.8 mb" -fi \
-#      -if ":225 mb" -set_lev "227.3 mb" -fi \
-#      -if ":275 mb" -set_lev "274.5 mb" -fi \
-#      -if ":300 mb" -set_lev "300.9 mb" -fi \
-#      -if ":350 mb" -set_lev "344.3 mb" -fi \
-#      -if ":400 mb" -set_lev "392.7 mb" -fi \
-#      -if ":450 mb" -set_lev "446.5 mb" -fi \
-#      -if ":500 mb" -set_lev "506 mb" -fi \
-#      -if ":600 mb" -set_lev "595.2 mb" -fi \
-#      -if ":700 mb" -set_lev "696.8 mb" -fi \
-#      -if ":750 mb" -set_lev "752.6 mb" -fi \
-#      -if ":800 mb" -set_lev "812 mb" -fi \
-#      -if ":850 mb" -set_lev "843.1 mb" -fi \
-#      -grib tmp_0p25_exact.grb2
-
-    # Filter limited levels according to ICAO standard
-    $WGRIB2 tmp_0p25_exact.grb2 | grep -F -f $FIXgfs/legend/wafs_gfsmaster.grb2_0p25.list \
-        | $WGRIB2 -i tmp_0p25_exact.grb2 -set master_table 25 -grib gfs.t${cyc}z.wafs_0p25_unblended.f${ffhr2}.grib2
-    $WGRIB2 -s gfs.t${cyc}z.wafs_0p25_unblended.f${ffhr2}.grib2 > gfs.t${cyc}z.wafs_0p25_unblended.f${ffhr2}.grib2.idx  
+    # Convert template 5 to 5.40
+    #$WGRIB2 tmp_wafs_0p25.grb2.forblend -set_grib_type jpeg -grib_out gfs.t${cyc}z.wafs_0p25_unblended.f${ffhr2}.grib2
+    mv tmp_wafs_0p25.grb2.forblend gfs.t${cyc}z.wafs_0p25_unblended.f${ffhr2}.grib2
+    $WGRIB2 -s gfs.t${cyc}z.wafs_0p25_unblended.f${ffhr2}.grib2 > gfs.t${cyc}z.wafs_0p25_unblended.f${ffhr2}.grib2.idx
 fi
-
-###### Step 6 TOCGIB2 ######
-# As in August 2020, no WMO header is needed for WAFS data at 1/4 deg
-## . prep_step
-## startmsg
-## export FORT11=gfs.t${cyc}z.wafs_0p25_unblended.f${ffhr2}.grib2
-## export FORT31=" "
-## export FORT51=gfs.t${cyc}z.wafs_0p25_unblended_wifs.f${ffhr2}.grib2
-## $TOCGRIB2 <  $FIXgfs/grib2_gfs_wafs_wifs_f${ffhr}.0p25 >> $pgmout 2> errfile
-## err=$?;export err ;err_chk
-## echo " error from tocgrib2=",$err
 
 if [ $SENDCOM = "YES" ] ; then
 
