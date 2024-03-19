91b68bdca6cfd63bee17ab97a4824591299ce370
Rahul Mahajan <aerorahul@users.noreply.github.com>
2024-03-18 22:22:11 -0400

    initial clean-up in preparation for merging to global-workflow

--- modulefiles/wafs_wcoss2.intel.lua
+++ modulefiles/wafs_wcoss2.intel.lua
@@ -12,6 +12,13 @@ local jasper_ver=os.getenv("jasper_ver") or "2.0.25"
 local zlib_ver=os.getenv("zlib_ver") or "1.2.11"
 local libpng_ver=os.getenv("libpng_ver") or "1.6.37"
 
+local bufr_ver=os.getenv("bufr_ver") or "11.7.0"
+local bacio_ver=os.getenv("bacio_ver") or "2.4.1"
+local w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
+local sp_ver=os.getenv("sp_ver") or "2.3.3"
+local ip_ver=os.getenv("ip_ver") or "3.3.3"
+local g2_ver=os.getenv("g2_ver") or "3.4.5"
+
 load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
 load(pathJoin("intel", intel_ver))
 load(pathJoin("craype", craype_ver))
@@ -22,6 +29,11 @@ load(pathJoin("jasper", jasper_ver))
 load(pathJoin("zlib", zlib_ver))
 load(pathJoin("libpng", libpng_ver))
 
-load("wafs_common")
+load(pathJoin("bufr", bufr_ver))
+load(pathJoin("bacio", bacio_ver))
+load(pathJoin("w3emc", w3emc_ver))
+load(pathJoin("sp", sp_ver))
+load(pathJoin("ip", ip_ver))
+load(pathJoin("g2", g2_ver))
 
 whatis("Description: WAFS environment on WCOSS2 with Intel Compilers")
