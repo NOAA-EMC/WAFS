87b37c18c7d448100184846934217e11ed5e99e0
yali mao <yali.mao@clogin06.cactus.wcoss2.ncep.noaa.gov>
2024-05-24 21:18:02 +0000

    Add sorc/checkout_upp.sh

---
+++ sorc/checkout_upp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+
+# Get the root of the cloned WAFS directory
+readonly DIR_ROOT=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )/.." && pwd -P)
+
+# Checkout upp code
+git -c submodule."sorc/upp.fd".update=checkout submodule update --init --recursive
+
+# Checkout upp/sorc/post_gtg code
+cd "${DIR_ROOT}/sorc/upp.fd"
+git -c submodule."post_gtg.fd".update=checkout submodule update --init --recursive
