20084de74eadfd8525f36d13dd6d7e7f6e1da8f9
yali mao <yali.mao@clogin04.cactus.wcoss2.ncep.noaa.gov>
2024-05-29 23:10:31 +0000

    1. Reset UPP to v8.3.0 2. Modified checkout_upp.sh procedure for UPP v8.3.0 3. Added build.ver and modified modulefiles/wafs_wcoss2.intel.lua for wafs 4. Modified build_upp.sh and build_wafs.sh 5. Removed wafs_grib2_0p25.fd which was for ICAO standard vertical levels and will not be used anymore

--- dev/modulefiles/upp.sh
+++ dev/modulefiles/upp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# modulefile for WAFS run
+# modulefile for EVS wafs stat
 set -x
 
 module load intel/${intel_ver}
@@ -7,9 +7,9 @@ module load PrgEnv-intel/${PrgEnvintel_ver}
 module load craype/$craype_ver
 
 # To access mpiexec
-# module load cray-pals/$craypals_ver
+module load cray-pals/$craypals_ver
 # For MPMD
-# module load cftp/$cfp_ver
+module load cftp/$cfp_ver
 
 module load libjpeg/$libjpeg_ver
 module load prod_util/$prod_util_ver
@@ -17,8 +17,8 @@ module load prod_envir/$prod_envir_ver
 module load grib_util/$grib_util_ver
 module load wgrib2/$wgrib2_ver
 
-# For make_NTC_file.pl in blending
-# module load util_shared/$util_shared_ver
-
-# For GCIP
-# module load bufr_dump/$bufr_dump_ver
+module load hdf5/$hdf5_ver
+module load netcdf/$netcdf_ver
+module load crtm/$crtm_ver
+module load cray-mpich/$craympich_ver
+module load g2tmpl/$g2tmpl_ver
--- sorc/build_upp.sh
+++ sorc/build_upp.sh
@@ -5,28 +5,17 @@ set -eu
 # Get the root of the cloned WAFS directory
 readonly DIR_ROOT=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )/.." && pwd -P)
 
-# User Options
-BUILD_TYPE=${BUILD_TYPE:-"Release"}
-
-# Build upp.x
-cd "${DIR_ROOT}/sorc/upp.fd/tests"
-
-_opts="-g "  # Always build with GTG ON
-_opts+="-p ${DIR_ROOT}/sorc/install/upp "  # Install prefix
-[[ "${BUILD_TYPE:-}" == "Debug" ]] && _opts+="-d "
-[[ "${BUILD_VERBOSE:-}" =~ [yYtT] ]] && _opts+="-v "
-
-export BUILD_DIR="${DIR_ROOT}/sorc/build/upp"
-
-BUILD_JOBS=${BUILD_JOBS:-8} ./compile_upp.sh ${_opts}
-
 # Check WAFS/exec folder exists
 if [[ ! -d "${DIR_ROOT}/exec" ]]; then
   mkdir -p "${DIR_ROOT}/exec"
 fi
 
-# Copy upp.x to WAFS/exec
-rm -rf "${DIR_ROOT}/exec/upp.x"
-cp "${DIR_ROOT}/sorc/install/upp/bin/upp.x" "${DIR_ROOT}/exec/upp.x"
+# Build upp executable file
+cd "${DIR_ROOT}/sorc/upp.fd/sorc"
+sh build_ncep_post.sh
+
+# Copy upp to WAFS/exec
+rm -rf "${DIR_ROOT}/exec/ncep_post"
+cp "${DIR_ROOT}/sorc/upp.fd/exec/ncep_post" "${DIR_ROOT}/exec/ncep_post"
 
 exit
