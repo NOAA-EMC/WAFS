9ef7d2f09efebb94d37cae5bca10baca417040fd
yali mao <yali.mao@clogin03.cactus.wcoss2.ncep.noaa.gov>
2024-10-05 17:57:57 +0000

    Update ecflow after switching blending to MPMD parallel run. 1. Don't need to setup ecflow links for blending 2. In ecflow definations, change event triggers of each forecast hour to f048 of the upstream completion Change COMROOT from 'com' to '%ENVIR%/com'

--- ecf/setup_ecf_links.sh
+++ ecf/setup_ecf_links.sh
@@ -66,12 +66,6 @@ fhrs="${seq1} ${seq2} ${seq3}"
 link_master_to_fhr "jwafs_grib2_0p25" "${fhrs}" "${CLEAN}"
 
 # JWAFS_BLENDING_0P25
-cd "${ECF_DIR}/scripts/grib2/0p25/blending"
-echo "Linking grib2/0p25/blending ..."
-seq1=$(seq -s ' ' 6 1 24)  # 006 -> 024; 1-hourly
-seq2=$(seq -s ' ' 27 3 48) # 027 -> 048; 3-hourly
-fhrs="${seq1} ${seq2}"
-link_master_to_fhr "jwafs_grib2_0p25_blending" "${fhrs}" "${CLEAN}"
 
 # JWAFS_GCIP
 cd "${ECF_DIR}/scripts/gcip"
