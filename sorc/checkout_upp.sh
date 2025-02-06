61c2d93e0dc2a2c7987d994959996e75c6349cc2
yali mao <yali.mao@dlogin03.dogwood.wcoss2.ncep.noaa.gov>
2025-02-06 13:14:56 +0000

    - Updated UPP to develop branch, the most recent version. - Modified the checkout procedure for UPP/GTG submoddules - Modfiled the compiling procedure for UPP/GTG submoddules

--- sorc/checkout_upp.sh
+++ sorc/checkout_upp.sh
@@ -8,4 +8,29 @@ readonly DIR_ROOT=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )/.."
 cd "${DIR_ROOT}"
 
 # Checkout upp and gtg code
-git submodule update --init --recursive
+git -c submodule."post_gtg.fd".update=checkout submodule update --init --recursive
+
+# upp:
+cd "${DIR_ROOT}/sorc/wafs_upp.fd"
+
+# copy WAFS specific UPP parm/ files to the main vertical structure
+mkdir -p "${DIR_ROOT}/parm/upp"
+upp_parm_files=(postcntrl_gfs_wafs_anl.xml \
+                postcntrl_gfs_wafs_ext.xml \
+                postcntrl_gfs_wafs.xml \
+                postxconfig-NT-gfs-wafs-anl.txt \
+                postxconfig-NT-gfs-wafs-ext.txt \
+                postxconfig-NT-gfs-wafs.txt)
+for upp_parm_file in "${upp_parm_files[@]}"; do
+  rm -f "${DIR_ROOT}/parm/upp/${upp_parm_file}"
+  cp "parm/gfs/${upp_parm_file}" "${DIR_ROOT}/parm/upp/${upp_parm_file}"
+done
+gtg_parm_files=(gtg.config.gfs \
+		gtg.input.gfs \
+                imprintings.gtg_gfs.txt)
+for gtg_parm_file in "${gtg_parm_files[@]}"; do
+    rm -f "${DIR_ROOT}/parm/upp/${gtg_parm_file}"
+    cp "sorc/ncep_post.fd/post_gtg.fd/${gtg_parm_file}" "${DIR_ROOT}/parm/upp/${gtg_parm_file}"
+done
+
+exit
